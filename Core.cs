7f96b93b1ff5dd7821c22bad28320fe5b75c0dcd
Luan Ferreira <luan.ferreira.jorge@gmail.com>
2022-04-20 20:57:39 -0300

    Comments and summaries

--- Core.cs
+++ Core.cs
@@ -9,7 +9,6 @@ namespace GameOf24
         /// <summary>
         /// Splits the string at every comma
         /// </summary>
-        /// <param name="input"></param>
         /// <returns>Array containing input numbers separated by commas</returns>
         internal static int[] SplitInput(string input)
         {
@@ -31,13 +30,6 @@ namespace GameOf24
         /// <summary>
         /// Stores function in functionList for later processing
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="prefix"></param>
-        /// <param name="op1">First symbol</param>
-        /// <param name="op2">Second symbol</param>
-        /// <param name="op3">Third symbol</param>
-        /// <param name="suffix"></param>
-        /// <param name="functionsList"></param>
         private static void StoreFunction(int[] input, string prefix, string op1, string op2, string op3, string suffix,
             List<string> functionsList)
         {
@@ -48,13 +40,14 @@ namespace GameOf24
         /// <summary>
         /// Try every written operations for every possible arrangement of the input array
         /// </summary>
-        /// <param name="input">Array with the 4 numbers to be tried</param>
+        /// <param name="input">Array with the 4 numbers to be used on functions</param>
         /// <param name="goal">Number that the 4 arrays numbers must reach</param>
         internal static void TryOperationsInEveryPermutation(int[] input, int goal)
         {
             List<string> solutions = new();
-            //Trying all operations for every possible randomization of the input list
-            input.ForEachPermutation((inputPermut) => TryAllOperations(goal, inputPermut, solutions));
+            
+            //Calling TryAllOperations for each permutation of input
+            input.ForEachPermutation(inputPermut => TryAllOperations(goal, inputPermut, solutions));
 
             //Solution not possible
             if (solutions.Count == 0)
@@ -72,13 +65,63 @@ namespace GameOf24
                 solutions.ForEach(s => Console.WriteLine(s + "\n"));
             }
         }
+        
+        /// <summary>
+        /// Heap's algorithm to find all permutations. Non recursive, more efficient.
+        /// Source: https://stackoverflow.com/questions/11208446/generating-permutations-of-a-set-most-efficiently
+        /// </summary>
+        /// <param name="items">Items to permute in each possible ways</param>
+        /// <param name="callback">Method to be called for each permutation</param>
+        private static void ForEachPermutation<T>(this T[] items, Action<T[]> callback)
+        {
+            // Swap 2 elements of same type
+            void Swap<T>(ref T a, ref T b)
+            {
+                T temp = a;
+                a = b;
+                b = temp;
+            }
+
+            int countOfItem = items.Length;
+
+            if (countOfItem <= 1)
+            {
+                callback(items);
+            }
+
+            int[] indexes = new int[countOfItem];
+
+
+            callback(items);
+
+
+            for (int i = 1; i < countOfItem;)
+            {
+                if (indexes[i] < i)
+                {
+                    if ((i & 1) == 1)
+                    {
+                        Swap(ref items[i], ref items[indexes[i]]);
+                    }
+                    else
+                    {
+                        Swap(ref items[i], ref items[0]);
+                    }
+
+                    callback(items);
+                    indexes[i]++;
+                    i = 1;
+                }
+                else
+                {
+                    indexes[i++] = 0;
+                }
+            }
+        }
 
         /// <summary>
         /// Try multiple math functions and store the functions in solutions list when successful
         /// </summary>
-        /// <param name="goal"></param>
-        /// <param name="input"></param>
-        /// <param name="solutions"></param>
         private static void TryAllOperations(int goal, int[] input, List<string> solutions)
         {
             if (input[0] + input[1] + input[2] + input[3] == goal)
@@ -229,60 +272,6 @@ namespace GameOf24
             if (input[0] * input[1] == goal * input[2] * input[3])
             {
                 StoreFunction(input, "( ", " * ", " / ", " ) / ", "", solutions);
-            }
-        }
-
-        /// <summary>
-        /// Heap's algorithm to find all permutations. Non recursive, more efficient.
-        /// Source: https://stackoverflow.com/questions/11208446/generating-permutations-of-a-set-most-efficiently
-        /// </summary>
-        /// <param name="items">Items to permute in each possible ways</param>
-        /// <param name="callback">Method to be called for each permutation</param>
-        private static void ForEachPermutation<T>(this T[] items, Action<T[]> callback)
-        {
-            // Swap 2 elements of same type
-            void Swap<T>(ref T a, ref T b)
-            {
-                T temp = a;
-                a = b;
-                b = temp;
-            }
-
-            int countOfItem = items.Length;
-
-            if (countOfItem <= 1)
-            {
-                callback(items);
-            }
-
-            int[] indexes = new int[countOfItem];
-
-
-            callback(items);
-
-
-            for (int i = 1; i < countOfItem;)
-            {
-                if (indexes[i] < i)
-                {
-                    // On the web there is an implementation with a multiplication which should be less efficient.
-                    if ((i & 1) == 1) // if (i % 2 == 1)  ... more efficient ??? At least the same.
-                    {
-                        Swap(ref items[i], ref items[indexes[i]]);
-                    }
-                    else
-                    {
-                        Swap(ref items[i], ref items[0]);
-                    }
-
-                    callback(items);
-                    indexes[i]++;
-                    i = 1;
-                }
-                else
-                {
-                    indexes[i++] = 0;
-                }
             }
         }
     }
